0e70fca8adeba589fe6d88cef0829fc593852cd5
projesilvestrini <100374678+projesilvestrini@users.noreply.github.com>
2023-03-29 14:32:25 -0300

    Add files via upload

---
+++ exemplo/produto.cs
@@ -0,0 +1,19 @@
+﻿namespace exemplo;
+class Produto
+{
+    public string? Nome {get;set;}
+
+
+    public double Preco {get;set;}
+
+    public void Mostradados(){
+        Console.WriteLine("nome: " + this.Nome);
+        Console.WriteLine("preco: " + this.Preco);
+    }
+    public void AlteraNome(string Nome){
+        this.Nome = Nome;
+    }
+    public void AlteraPreco(double preco){
+        this.Preco = preco;
+    }
+}
