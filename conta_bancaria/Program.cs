3a401186a05ecaddcc1afd7e1496674e2f68bd6d
projesilvestrini <100374678+projesilvestrini@users.noreply.github.com>
2023-04-05 14:44:44 -0300

    Add files via upload

---
+++ conta_bancaria/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace conta_bancaria;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Exercicio Conta Bancária - ETEC MCM");
+        Conta conta = new Conta();
+
+        conta.nomeCliente = "Brian Souza Dos Santos";
+        //conta.numeroConta = 123;
+        conta.limite = 500;
+        //conta.saldo = 5000;
+        conta.depositar(200);
+        conta.sacar(50);
+
+        //Console.WriteLine("Olá, " + conta.nomeCliente + ", seu saldo é de: " + conta.saldo);
+        //Console.WriteLine("Seu limite é de: " + conta.limite);
+        //Console.WriteLine("O número da sua conta é: " + conta.numeroConta);
+
+
+        // Chamar o método ConsultaSaldo e receber em uma variável local chamada saldo o valor disponível em saldo desse cliente
+        double saldo = conta.ConsultaSaldo();
+        
+        // Visualizar:
+        Console.WriteLine("Seu saldo é de: " + saldo);
+    }
+}
