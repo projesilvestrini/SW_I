2c22e5c897d50dab6609434f43709654ed3b2e44
projesilvestrini <100374678+projesilvestrini@users.noreply.github.com>
2023-05-27 10:23:38 -0300

    Add files via upload

---
+++ ex 2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ex_2;
+class Program
+{
+    static void Main(string[] args)
+    {
+       ﻿namespace ex2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+    }
+}
